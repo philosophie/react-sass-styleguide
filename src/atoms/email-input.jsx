4d49d76cbbcc33a90941bb64b64c646a0e78ae19
Ashwin Vaswani <ashwinv11@gmail.com>
2019-11-25 11:05:21 -0800

    fix build

--- src/atoms/email-input.jsx
+++ src/atoms/email-input.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-import * as Form from 'react-bootstrap/Form'
+import Form from 'react-bootstrap/Form'
 
 export function EmailInput(props) {
   return (
-    <Form.Control placeholder={props.placeholder} size="lg" className={props.className ? props.className + " email-input" : "email-input"}/>
+    <Form.Control
+      placeholder={props.placeholder}
+      size="lg"
+      className={
+        props.className ? props.className + ' email-input' : 'email-input'
+      }
+    />
   )
 }
